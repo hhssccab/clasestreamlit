de73fca7629d67acdcb5116398e08ad19eb113c8
hhssccab <holmanc@uaz.edu.mx>
2024-06-27 19:38:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from utils import Data
 
-data = Data("https://github.com/hhssccab/clasestreamlit/blob/main/iris.csv")
+data = Data("https://raw.githubusercontent.com/hhssccab/clasestreamlit/main/iris.csv")
 flowers_variety = data.get_data()['variety'].unique()
 
 variety = st.selectbox(
